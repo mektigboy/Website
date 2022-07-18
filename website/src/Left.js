05996232652ffb2a2cd838e2b860c368177ff136
mektigboy <mektigboy@protonmail.com>
2022-07-17 21:01:33 -0500

    Update src/Left.js, src/Loader.css, src/Loader.js, src/Right.js

--- website/src/Left.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Left = () => {};
-
-export default Left;
--- website/src/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import "./Loader.css";
-
-const Loader = () => {
-  return <div>Loading...</div>;
-};
-
-export default Loader;
