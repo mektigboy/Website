670e1908532e2d9eff5f344021c509d13404e6af
mektigboy <mektigboy@protonmail.com>
2022-07-17 20:59:55 -0500

    Update README.md

---
+++ website/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Loader from "./Loader/Loader";
+
+class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return <Loader />;
+  }
+}
+
+export default App;
