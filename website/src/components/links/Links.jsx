f27860e6971dd628a596475843c229f6071acbe9
mektigboy <mektigboy@protonmail.com>
2022-07-20 21:09:15 -0500

    Update website/src/components/links/Links.jsx, website/src/index.css

---
+++ website/src/components/links/Links.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Links = () => {
+  return (
+    <div className="links">
+      <a href="https://" target="_blank">
+        email
+      </a>
+      <a href="https://" target="_blank">
+        github
+      </a>
+      <a href="https://" target="_blank">
+        mirror
+      </a>
+      <a href="https://" target="_blank">
+        twitter
+      </a>
+    </div>
+  );
+};
+
+export default Links;
