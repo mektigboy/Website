99db5d741ea5234abbb177cb37528abd6c14c037
mektigboy <mektigboy@protonmail.com>
2022-07-25 09:57:49 -0500

    Update website/src/components/loader/Loader.jsx

---
+++ website/src/components/loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+  return (
+    <section id="loader" className="loader">
+      <div className="loader-box">
+        <p>Loading...</p>
+      </div>
+    </section>
+  );
+};
+
+export default Loader;
