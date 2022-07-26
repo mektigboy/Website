cc9ca37cc7d8320c5e6d6d08768851af24ec259b
mektigboy <mektigboy@protonmail.com>
2022-07-26 13:53:09 -0500

    Update website/src/App.jsx

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Loader from "./components/loader/Loader";
-import Home from "/components/home/Home";
+import Home from "./components/home/Home";
 
 const App = () => {
   return <Home />;
---
+++ website/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section id="home" className="home">
+      <div></div>
+    </section>
+  );
+};
+
+export default Home;
