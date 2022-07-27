9f13b7841a4398f52c631d99e76e7efe81e8d497
mektigboy <mektigboy@protonmail.com>
2022-07-27 11:12:38 -0500

    Update src/components/home/Right.jsx

--- website/src/components/home/Right.jsx
+++ website/src/components/home/Right.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-const Right = () => {};
+const Right = () => {
+  return (
+    <div className="right">
+      <div className="right-box">
+        <div className="right-top">
+          mektigboy
+          <a href="">connect wallet</a>
+        </div>
+        <div className="right-middle">
+          blockchain developer / security researcher.
+        </div>
+        <div className="right-bottom">
+          view experiments
+          <div className="right-bottom-box">
+            <ul>
+              <li className="mb-10">
+                <a href="mailto:me@mektigboy.com" target="_blank">email</a>
+              </li>
+              <li className="mb-10">
+                <a href="https://github.com/mektigboy" target="_blank">github</a>
+              </li>
+              <li className="mb-10">
+                <a href="https://mirror.xyz/mektigboy.eth" target="_blank">mirror</a>
+              </li>
+              <li>
+                <a href="https://twitter.com/mektigboy" target="_blank">twitter</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Right;
